74a1dcabfbb84a03ce50e0e626e1675186f25c5a
rexi1r <yousofzadeh.h@gmail.com>
2025-08-10 09:10:57 +0330

    fix postcss

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  plugins: [
-    require("@tailwindcss/postcss"),
-    require("autoprefixer"),
-  ],
+  plugins: {
+    "@tailwindcss/postcss": {}, // پلاگین جدید PostCSS برای Tailwind v4
+    tailwindcss: {}, // خود فریم‌ورک Tailwind
+    autoprefixer: {}, // اختیاری: اگر می‌خواهید همچنان از autoprefixer استفاده کنید
+  },
 };
+
